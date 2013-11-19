d4e5891b196c1465f74ee0aa1721a73f699ac4da
Josephine Gianni <josephineg@hotmail.com>
2013-11-19 18:47:24 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuffFiner::Application.config.secret_key_base = 'e861837ebcc99dd08b4be8bbea353d0bcb7b150d62eb652c9d8399aca15d9bf880dd65ae96ab4d5274fe0344630298656e7509f403aceb8c34fbef81812af9b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StuffFiner::Application.config.session_store :cookie_store, key: '_stuff_finer_session'
